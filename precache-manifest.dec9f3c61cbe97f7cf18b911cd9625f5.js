31bbe6fabc56e1b6ea1c32ee26f59b3319ea8861
Atiya Haider <atiya_haider@hotmail.com>
2020-06-07 12:17:03 -0400

    Updates

---
+++ precache-manifest.dec9f3c61cbe97f7cf18b911cd9625f5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c6ff2a031c4133ad67b8cb8aac2e387",
+    "url": "/Memory-Match-Game/index.html"
+  },
+  {
+    "revision": "b52e9a29911ae73f009f",
+    "url": "/Memory-Match-Game/static/css/main.f0a7048a.chunk.css"
+  },
+  {
+    "revision": "b4d1db9871c72ee2fe31",
+    "url": "/Memory-Match-Game/static/js/2.a842696a.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/Memory-Match-Game/static/js/2.a842696a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b52e9a29911ae73f009f",
+    "url": "/Memory-Match-Game/static/js/main.82fffdf4.chunk.js"
+  },
+  {
+    "revision": "27ee6c4a90e53dd4e862",
+    "url": "/Memory-Match-Game/static/js/runtime-main.e9cddfc2.js"
+  },
+  {
+    "revision": "7fa8985a45b1dc3e134699cfca1df38d",
+    "url": "/Memory-Match-Game/static/media/ApexLakeRegular-LPaD.7fa8985a.ttf"
+  },
+  {
+    "revision": "282c443c0df826454972b40f2a3a981a",
+    "url": "/Memory-Match-Game/static/media/JiHiddenVines-PZZB.282c443c.ttf"
+  }
+]);
